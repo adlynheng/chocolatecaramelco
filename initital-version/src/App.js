c89e983fef1cd562bfa99d493e5f183d223d5226
Adlyn Heng <adlynheng@gmail.com>
2022-04-10 00:27:05 +0800

    Add src files.

---
+++ initital-version/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Header } from "./Components/Header";
+import { Navbar } from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
